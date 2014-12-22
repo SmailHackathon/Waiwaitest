4069c5eb97f95ea62677634df68e285b2c54a2ac
makky0620 <b1013059@gmail.com>
2014-12-22 22:59:09 +0900

    トップページからのカメラ

--- src/to/msn/wings/camerabasic/CameraActivity.java
+++ src/to/msn/wings/camerabasic/CameraActivity.java
@@ -1,5 +1,19 @@
 package to.msn.wings.camerabasic;
 
-public class CameraActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class CameraActivity extends Activity{
 	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	      super.onCreate(savedInstanceState);
+	/*        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+	        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
+	      setContentView(new CameraSurface(this));
+	    }
+
+	    
+
 }
--- src/to/msn/wings/camerabasic/MainActivity.java
+++ src/to/msn/wings/camerabasic/MainActivity.java
@@ -1,22 +1,39 @@
 package to.msn.wings.camerabasic;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	Button bt;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
 /*        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
-      setContentView(new CameraSurface(this));
+      setContentView(R.layout.activity_main);
+      
+      
+      bt = (Button)findViewById(R.id.button);
+      bt.setOnClickListener(new SampleClickListener());
     }
+    
+    class SampleClickListener implements OnClickListener{
+    	public void onClick(View v){
+    		if(v == bt){
+        	Intent i = new Intent(getApplication(), to.msn.wings.camerabasic.CameraActivity.class);
+        	//アクティビティの開始
+        	startActivity(i);
+    		}
+    	}
+    }
+    
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+   
 }
